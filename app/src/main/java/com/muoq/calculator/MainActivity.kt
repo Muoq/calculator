c9dba93e50df3b9ceea5780fa0061563f2762e42
Neutrinoq <keilvictor60@gmail.com>
2017-09-26 21:17:37 +0300

    start working on gui

--- app/src/main/java/com/muoq/calculator/MainActivity.kt
+++ app/src/main/java/com/muoq/calculator/MainActivity.kt
@@ -4,12 +4,7 @@ import android.graphics.Path
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.muoq.calculator.logic.Expression
-import com.muoq.calculator.logic.Operator
-import com.muoq.calculator.logic.Solver
-import java.math.BigDecimal
-import java.math.MathContext
-import java.math.RoundingMode
+import android.widget.GridView
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,62 +15,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//
-//        Log.i(TAG, "exp1 size: " + exp1.mutableExpression.size.toString())
-//
-//        var logString = ""
-//
-//        exp1.expression.forEach {logString += it}
-//
-//        Log.i(TAG, logString)
-//
-//        println("\n===========")
-//
-////        Log.i(TAG, "===========")
-//
-//        val answer = exp1.solve()
-//
-//        logString = ""
-//
-//        exp1.mutableExpression.forEach {logString += it}
-//
-//        Log.i(TAG, logString)
-//
-//        Log.i(TAG, "Answer: " + answer)
-//
-//        Log.i(TAG, "exp1 size: " + exp1.mutableExpression.size.toString())
-
-        var op1: Operator = Operator(Operator.DIVIDE)
-
-        Log.i(TAG, op1.operation(BigDecimal(8), BigDecimal(13)).toString())
-
-        Log.i(TAG, op1.hierarchy.toString())
-
-//        var exp1 = Expression()
-//
-//        exp1.addOperator(Operator(Operator.ADD))
-//        exp1.addNumber(BigDecimal(314))
-//        exp1.addNumber(BigDecimal(315))
-//        exp1.addOperator(Operator(Operator.MULTIPLY))
-//        exp1.addOperator(Operator(Operator.DIVIDE))
-//        exp1.addOperator(Operator(Operator.O_PARENTHESIS))
-//        exp1.addOperator(Operator(Operator.SUBTRACT))
-//
-//        exp1.setNumber(2, BigDecimal(6))
-//        exp1.setNumber(1, BigDecimal(5))
-
-        val exp1 = Expression("7+8-23*(19/8*(12 + 3 + 14*2) - 25) + 31*4*5*9/2/8")
-
-        Log.i(TAG, exp1.toString())
-
-        exp1.solve()
-
-//        Log.i(TAG, (exp1.expression[0][1] is Operator).toString())
-
-        Log.i(TAG, exp1.toString() + " LAST")
-
-        Log.i(TAG, exp1.solution.setScale(6, RoundingMode.HALF_UP).toString())
-
     }
 }
---
+++ app/src/main/java/com/muoq/calculator/ui/SquareButton.kt
@@ -0,0 +1,25 @@
+package com.muoq.calculator.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.Button
+
+class SquareButton: Button {
+
+    constructor(context: Context): super(context)
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val width: Int = MeasureSpec.getSize(widthMeasureSpec)
+        setMeasuredDimension(width, width)
+
+    }
+
+}
